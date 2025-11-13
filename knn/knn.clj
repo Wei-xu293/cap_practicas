b1bc4c1a684b85967b7ef1d496c7fa6a573e65ce
Wei-xu293 <falseacc007@gmail.com>
2025-11-13 23:38:45 +0100

    tests

--- knn/knn.clj
+++ knn/knn.clj
@@ -15,3 +15,10 @@
     (m/sqrt (fold #(+ (* (- (first %1) (second %1)) (- (first %1) (second %1))) %2) 0 u))))
 
 ;(use 'knn :reload-all)
+
+(println (de [1 0] [1 1]))
+(println (de [0 0 0] [1 1 1]))
+;(println (dm [1 0] [1 1]))
+;(println (dm [0 0 0] [1 1 1]))
+;(println (dh [:a :b] [:b :b]))
+;(println (dh [:a :a :a] [:b :b :b]))
