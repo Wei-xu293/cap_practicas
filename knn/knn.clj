6fcac78b6748332d1af7e686ab470db6a1830a45
Wei-xu293 <falseacc007@gmail.com>
2025-11-16 00:04:06 +0100

    hamming

--- knn/knn.clj
+++ knn/knn.clj
@@ -14,11 +14,17 @@
   (let [u (map vector v w)]
     (m/sqrt (fold #(+ (* (- (first %1) (second %1)) (- (first %1) (second %1))) %2) 0 u))))
 
+(defn dh [v w]
+  (let [n (count v) m (count w) u (map vector v w)]
+    (assert (= n m))
+    (/ (reduce #(if (= (first %2) (second %2)) %1 (inc %1)) 0 u) n)))
+
 ;(use 'knn :reload-all)
+
 
 (println (de [1 0] [1 1]))
 (println (de [0 0 0] [1 1 1]))
 ;(println (dm [1 0] [1 1]))
 ;(println (dm [0 0 0] [1 1 1]))
-;(println (dh [:a :b] [:b :b]))
-;(println (dh [:a :a :a] [:b :b :b]))
+(println (dh [:a :b] [:b :b]))
+(println (dh [:a :a :a] [:b :b :b]))
